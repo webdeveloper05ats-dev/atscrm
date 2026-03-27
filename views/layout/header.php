9c8e1c6df7c7c731db3ca194414ad2225cc6fe99
webdeveloper05ats-dev <webdeveloper05.ats@gmail.com>
2026-03-27 12:40:36 +0530

    hea

--- views/layout/header.php
+++ views/layout/header.php
@@ -33,6 +33,8 @@ $hideTopbar = (isset($hideSidebar) && $hideSidebar === true);
 <!-- Google Font: Poppins -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://cdnjs.cloudflare.com" crossorigin>
+<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
 
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <!-- Main CSS -->
