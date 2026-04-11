eba20939f2dbf5d3409b9cc300d90be2cba0f8c1
webdeveloper05ats-dev <webdeveloper05.ats@gmail.com>
2026-04-11 16:24:16 +0530

    ach

--- views/leads/add.php
+++ views/leads/add.php
@@ -260,7 +260,7 @@ alert('<?= addslashes($error) ?>');
 </div>
 
 <div class="lead-card-body">
-<form method="POST" id="leadForm" novalidate data-focus-start="on" data-focus-target="input[name='name']">
+<form method="POST" id="leadForm" novalidate data-focus-start="on" data-focus-target="input[name='name']" data-form-assist="on">
 
 <input type="hidden" name="csrf_token" value="<?= h(generateCSRF()) ?>">
 
